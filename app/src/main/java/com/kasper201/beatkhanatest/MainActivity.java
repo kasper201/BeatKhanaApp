4b6631a047c5906f659a51269c74279237b99909
Kasper <kasperjnssen@gmail.com>
2025-03-17 21:18:57 +0100

    move to github

---
+++ app/src/main/java/com/kasper201/beatkhanatest/MainActivity.java
@@ -0,0 +1,92 @@
+package com.kasper201.beatkhanatest;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.navigation_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setTitle("");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu); // Inflate the menu
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        if (searchItem != null) {
+            SearchView searchView = (SearchView) searchItem.getActionView();
+            if (searchView != null) {
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        searchView.clearFocus();
+                        // Do something with the query
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        // Do something with the query
+                        return true;
+                    }
+                });
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_home) { // using if-else statements as a workaround for the switch statement
+            // Do something for the "Home" action
+            return true;
+        } else if (id == R.id.action_recent) {
+            // Do something for the "Recent" action
+            return true;
+        } else if (id == R.id.action_saved_players) {
+            // Do something for the "Saved Players" action
+            return true;
+        } else if (id == R.id.action_search) {
+            // Do something for the "Search" action
+            return true;
+        } else if (id == R.id.action_player_profile) {
+            // Do something for the "Profile" action
+            return true;
+        } else {
+            // The user's action isn't recognized.
+            // Invoke the superclass to handle it.
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
