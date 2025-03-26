860248d18b8d6d48c1f5987aa38de48f38f23216
Kasper <kasperjnssen@gmail.com>
2025-03-26 22:55:08 +0100

    Refactor layout and add new fragments for improved navigation

--- app/src/main/java/com/kasper201/beatkhanatest/MainActivity.java
+++ app/src/main/java/com/kasper201/beatkhanatest/MainActivity.java
@@ -23,15 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.navigation_toolbar);
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().hide();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
